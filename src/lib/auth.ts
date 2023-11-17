3b37d9509dcb8e6177d04ac312e059b3a0f460c1
bensuz <elifbensuaslan@gmail.com>
2023-11-17 14:59:03 +0000

    authOptions export changed again

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,6 +6,7 @@ import { PrismaClient } from "@prisma/client";
 import { NextAuthOptions } from "next-auth";
 import { Adapter } from "next-auth/adapters";
 import GoogleProvider from "next-auth/providers/google";
+import NextAuth from "next-auth/next";
 
 export const authOptions: NextAuthOptions = {
     adapter: PrismaAdapter(prisma as PrismaClient) as Adapter,
@@ -27,3 +28,6 @@ export const authOptions: NextAuthOptions = {
         },
     },
 };
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
